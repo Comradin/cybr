f7d9e04a98f1170863c6ced68e5ad183f923a10f
Marcus Franke <marcus.franke@gmail.com>
2018-04-22 22:05:35 +0200

    [draft] initial flow concept
    Added a first JSON config struct and unmarshaling a first
    example JSON into this struct to perform the initial tcp
    check with.

--- main.go
+++ main.go
@@ -1,18 +1,44 @@
 package main
 
 import (
-	"net"
+	"encoding/json"
+	"fmt"
 	"log"
+	"net"
 )
+
+type Check struct {
+	Name string `json:"name"`
+	Type string `json:"type"`
+	Host string `json:"host"`
+	Port int    `json:"port"`
+}
 
 func main() {
 
-	// Simple TCP Check
-	conn, err := net.Dial("tcp", "localhost:8081")
+	// raw json config
+	raw := `
+{
+  "Name": "Simple TCP",
+  "type": "TCP",
+  "host": "localhost",
+  "port": 8081
+}
+`
+
+	// create an Check instance and unmarshal the raw json into
+	simpletcp := Check{}
+	err := json.Unmarshal([]byte(raw), &simpletcp)
 	if err != nil {
-		log.Println( "CYBR - You cannot be reached!")
+		log.Fatal("Could not read config")
+	}
+
+	// Simple TCP Check
+	conn, err := net.Dial(simpletcp.Type, fmt.Sprintf("%s:%s", simpletcp.Host, simpletcp.Port))
+	if err != nil {
+		log.Printf("CYBR (%s) - You cannot be reached!\n", simpletcp.Name)
 	} else {
-		log.Println("CYBR - You can be reached!")
+		log.Printf("CYBR (%s) - You can be reached!", simpletcp.Name)
 		conn.Close()
 	}
 }
